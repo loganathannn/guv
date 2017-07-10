51b8005dde3d9c04ddef6dacaa9814b40ffdc75f
loganathannn <loguhari09@gmail.com>
2017-07-10 13:57:08 +0530

    Create Stringtoint.java

---
+++ Stringtoint.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Stringtoint {
+	public static void main(String[] args)
+	{
+	Scanner sc= new Scanner(System.in);
+	String n =sc.nextLine();
+	int nu = Integer.valueOf(n);
+	System.out.println("The number is: " + nu);
+}
+}
